0acd64e43eb1ce786b64a0f47c5ce5df14e1147a
Devesh Rajput <devesh.rajput22@gmail.com>
2016-10-30 15:42:35 +0530

    MaxSubarrayK

---
+++ src/arrays/MaxSubarrayK.java
@@ -0,0 +1,49 @@
+package arrays;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MaxSubarrayK extends ArrayPrinter{
+
+	public MaxSubarrayK() throws FileNotFoundException {
+		super();
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		new MaxSubarrayK().start();
+	}
+	
+	public void start(){
+		int k = sc.nextInt();
+		sc.close();
+		printArray(arr);
+		
+		Deque<Integer> de = new ArrayDeque<Integer>();
+		int i;
+		for(i=0;i<k;i++){
+			
+			while(!de.isEmpty() && arr[i] >= arr[de.peekLast()])
+				de.pollLast();
+			
+			de.addLast(i);
+		}
+		
+		for(;i<arr.length;i++){
+			
+			System.out.print(arr[de.getFirst()]+" ");
+			
+			while(!de.isEmpty() && de.getFirst() <= i-k)
+				de.removeFirst();
+			while(!de.isEmpty() && arr[i] >= arr[de.peekLast()])
+				de.pollLast();
+			
+			de.addLast(i);
+		}
+		
+		System.out.println(arr[de.getFirst()]+" ");
+		
+	}
+	
+}
