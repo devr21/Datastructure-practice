040d86517941049975a95d20912f88a2bbb27ea4
Devesh Rajput <devesh.rajput22@gmail.com>
2016-11-01 23:08:03 +0530

    Insertion Sort

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,5 +1,35 @@
 package sorting;
 
-public class InsertionSort {
+import java.io.FileNotFoundException;
 
+import arrays.ArrayPrinter;
+
+public class InsertionSort extends ArrayPrinter{
+
+	public InsertionSort() throws FileNotFoundException {
+		super();
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		new InsertionSort().start();
+	}
+	
+	public void start(){
+		
+		for(int i=1;i<arr.length;i++){
+			
+			int key = arr[i];
+			int j = i-1;
+			
+			while(j >= 0 && arr[j] > key){
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1] = key;
+		}
+		
+		printArray(arr);
+		
+	}
+	
 }
